7cfbf26e81cb85c36ea4cab6e2549edc33808063
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2022-10-31 11:33:48 +0500

    movie model done

---
+++ lib/models/movie_moel.dart
@@ -0,0 +1,64 @@
+class Movie {
+  final String name;
+  final String imagePath;
+
+  final String videoPath;
+
+  final String category;
+  final int year;
+  final Duration duration;
+  Movie({
+    required this.name,
+    required this.imagePath,
+    required this.videoPath,
+    required this.category,
+    required this.year,
+    required this.duration,
+  });
+
+  static List<Movie> movie = [
+    Movie(
+        name: "The Innoncent",
+        imagePath: "imagePath",
+        videoPath: "videoPath",
+        category: "Thriller",
+        year: 2022,
+        duration: Duration(hours: 1, minutes: 40)),
+    Movie(
+        name: "The Innoncent",
+        imagePath: "imagePath",
+        videoPath: "videoPath",
+        category: "Thriller",
+        year: 2022,
+        duration: Duration(hours: 1, minutes: 40)),
+    Movie(
+        name: "The Innoncent",
+        imagePath: "imagePath",
+        videoPath: "videoPath",
+        category: "Thriller",
+        year: 2022,
+        duration: Duration(hours: 1, minutes: 40)),
+    Movie(
+        name: "The Innoncent",
+        imagePath: "imagePath",
+        videoPath: "videoPath",
+        category: "Thriller",
+        year: 2022,
+        duration: Duration(hours: 1, minutes: 40)),
+    Movie(
+        name: "The Innoncent",
+        imagePath: "imagePath",
+        videoPath: "videoPath",
+        category: "Thriller",
+        year: 2022,
+        duration: Duration(hours: 1, minutes: 40)),
+    Movie(
+    
+        name: "The Innoncent",
+        imagePath: "imagePath",
+        videoPath: "videoPath",
+        category: "Thriller",
+        year: 2022,
+        duration: Duration(hours: 1, minutes: 40)),
+  ];
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,73 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: _CustomClipper(),
+          child: Container(
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+            color: const Color(0xFF000B49),
+            child: Center(
+                child: Text(
+              "Explore",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+            )),
+          ),
+        ),
+      ),
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:
+              const EdgeInsets.symmetric(horizontal: 20.0, vertical: 150.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                  text: TextSpan(
+                      style: Theme.of(context).textTheme.headline6,
+                      children: [
+                    TextSpan(
+                        text: "Featured ",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(fontWeight: FontWeight.bold)),
+                    const TextSpan(text: "Movies"),
+                  ])),
+              const SizedBox(
+                height: 20.0,
+              )
+            ],
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class _CustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+    var path = Path();
+    path.lineTo(0, height - 50);
+    path.quadraticBezierTo(width / 2, height, width, height - 50);
+    path.lineTo(width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
